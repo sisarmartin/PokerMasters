9a8124a5682cedceafe2d271eb01cfe0de10fc95
Cesar <cesarpoti95@gmail.com>
2018-05-23 18:00:22 +0200

    Version 008

--- PokerMasters/Hand.cs
+++ PokerMasters/Hand.cs
@@ -6,6 +6,15 @@ public class Hand
     // A list of cards has been made in the hand class and the AddCard method
     // has been created to add cards to the hand
     protected List<Card> cards = new List<Card>();
+    public bool isPair = false;
+    public bool isTwoPair = false;
+    public bool isThreeKind = false;
+    public bool isStraight = false;
+    public bool isFlush = false;
+    public bool isFullHouse = false;
+    public bool isFourKind = false;
+    public bool isStraightFlush = false;
+    public bool isRoyalFlush = false;
 
     public void AddCard(Card card)
     {
@@ -13,8 +22,53 @@ public class Hand
     }
 
     // Established position where stay the hand of player #1
-    public void Position()
+    public void HighCard()
     {
-        // To do
+        
+    }
+
+    public void Pair()
+    {
+
+    }
+
+    public void TwoPair()
+    {
+
+    }
+
+    public void ThreeKind()
+    {
+
+    }
+
+    public void Straight()
+    {
+
+    }
+
+    public void Flush()
+    {
+
+    }
+
+    public void FullHouse()
+    {
+
+    }
+
+    public void FourKind()
+    {
+
+    }
+
+    public void StraightFlush()
+    {
+
+    }
+
+    public void RoyalFlush()
+    {
+
     }
 }
